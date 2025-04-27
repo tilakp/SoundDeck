1e80ee38138ec64a326fd8c081c43fb39f7fe081
Tilak <1124584+tilakp@users.noreply.github.com>
2025-04-26 18:20:18 -0700

    Initial Commit

---
+++ SoundDeck/SoundDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundDeckApp.swift
+//  SoundDeck
+//
+//  Created by Tilak Patel on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoundDeckTests/SoundDeckTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoundDeckTests.swift
+//  SoundDeckTests
+//
+//  Created by Tilak Patel on 4/26/25.
+//
+
+import Testing
+@testable import SoundDeck
+
+struct SoundDeckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
